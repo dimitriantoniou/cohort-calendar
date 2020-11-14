69388d5dbbfb7f232c86051942fb35c8e8c6d2af
Dimitri <dma2012@gmail.com>
2020-11-14 11:28:19 -0600

    passed title attribute from sample controller into view

--- src/main/java/com/edimitri/cohortcalendar/controllers/SampleController.java
+++ src/main/java/com/edimitri/cohortcalendar/controllers/SampleController.java
@@ -1,6 +1,7 @@
 package com.edimitri.cohortcalendar.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SampleController {
     //request path: /sample
     @RequestMapping(value="")
-    public String sample(){
+    public String sample(Model model){
+
+        model.addAttribute("title","My Sample");
         return "sample/sample";
     }
 }
