b3a336f82b0850689c5efe2d0ae20970f11666df
Dimitri <dma2012@gmail.com>
2020-11-14 11:18:39 -0600

    added index.html template and mapped HelloController to it

--- src/main/java/com/edimitri/cohortcalendar/controllers/HelloController.java
+++ src/main/java/com/edimitri/cohortcalendar/controllers/HelloController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @RequestMapping(value="")
-    @ResponseBody
     public String index(){
-        return "Welcome to the Codeup Cohort Calendar";
+        return "index";
     }
 }
