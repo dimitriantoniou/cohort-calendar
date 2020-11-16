5c80790336448f2498810b1592c194d9228ea612
Dimitri <dma2012@gmail.com>
2020-11-16 07:13:50 -0600

    removed unnecessary id incrementer

--- src/main/java/com/edimitri/cohortcalendar/models/User.java
+++ src/main/java/com/edimitri/cohortcalendar/models/User.java
@@ -28,17 +28,12 @@ public class User {
 
 
     public User(int id, String email, String firstName, String lastName, String password, boolean isAdmin) {
-        this();
         this.id=id;
         this.firstName=firstName;
         this.lastName=lastName;
         this.email = email;
         this.password=password;
         this.isAdmin=isAdmin;
-    }
-    public User(){
-        id=nextId;
-        nextId++;
     }
 
     public String getFirstName() {
