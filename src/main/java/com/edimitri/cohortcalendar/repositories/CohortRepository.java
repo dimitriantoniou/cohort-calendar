a7478acbf3a92c79b967f9d10ec15d2c1754a6bf
Dimitri <dma2012@gmail.com>
2020-11-15 18:56:19 -0600

    created cohort repository

--- src/main/java/com/edimitri/cohortcalendar/repositories/CohortRepository.java
+++ src/main/java/com/edimitri/cohortcalendar/repositories/CohortRepository.java
@@ -1,4 +1,7 @@
 package com.edimitri.cohortcalendar.repositories;
 
-public class CohortRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CohortRepository extends JpaRepository {
+
 }
