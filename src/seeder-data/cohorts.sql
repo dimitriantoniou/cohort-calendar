6ff9265d052e90e9429a87a7b19ed2ca75081ea4
Dimitri <dma2012@gmail.com>
2020-11-16 07:35:30 -0600

    re-formatted naming scheme

--- src/seeder-data/cohorts.sql
+++ src/seeder-data/cohorts.sql
@@ -1,4 +1,4 @@
 use cohort_calendar_db;
 
-INSERT INTO cohorts(name, startDate, programType, campus, contactHours)
-VALUES ('Fortuna',1/13/2020,1,'SAT',670),('Ganymede',3/23/2020,1,'SAT',670);
+INSERT INTO cohorts(name, start_date, program_type, campus, contact_hours)
+VALUES ('Fortuna',1/13/2020,1,1,670),('Ganymede',3/23/2020,1,'SAT',670);
