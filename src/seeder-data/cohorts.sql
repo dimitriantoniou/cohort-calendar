3e57b12c7f9fa1957eac61f44776749d69975367
Dimitri <dma2012@gmail.com>
2020-11-17 23:14:01 -0600

    reformatted dates

--- src/seeder-data/cohorts.sql
+++ src/seeder-data/cohorts.sql
@@ -1,4 +1,4 @@
 use cohort_calendar_db;
 
 INSERT INTO cohorts(name, start_date, program_type, campus, contact_hours)
-VALUES ('Fortuna',1/13/2020,1,1,670),('Ganymede',3/23/2020,1,'SAT',670);
+VALUES ('Fortuna',2020-01-13,'Web Dev','SAT',670),('Ganymede',2020-03-23,'Web Dev','SAT',670);
