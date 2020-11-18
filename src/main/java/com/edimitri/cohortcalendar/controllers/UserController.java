29995ddb9ee0b8cabcc853ab4e044ddc510f6840
Dimitri <dma2012@gmail.com>
2020-11-17 23:07:07 -0600

    programmatic login after registering user and skip login if user is authenticated

--- src/main/java/com/edimitri/cohortcalendar/controllers/UserController.java
+++ src/main/java/com/edimitri/cohortcalendar/controllers/UserController.java
@@ -1,6 +1,14 @@
 package com.edimitri.cohortcalendar.controllers;
 
 import com.edimitri.cohortcalendar.models.User;
+import com.edimitri.cohortcalendar.models.UserWithRoles;
+import com.edimitri.cohortcalendar.repositories.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import com.edimitri.cohortcalendar.repositories.UserRepository;
@@ -9,14 +17,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Collections;
+
 @Controller
 public class UserController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private Roles roles;
 
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder=passwordEncoder;
+    @Autowired
+    public UserController(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            UserRepository userRezpository, PasswordEncoder passwordEncoder1, Roles roles
+    ) {
+        this.userRepository = userRezpository;
+        this.passwordEncoder = passwordEncoder1;
+        this.roles = roles;
     }
 
     @GetMapping("/sign-up")
@@ -29,12 +46,23 @@ public class UserController {
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         userRepository.save(user);
-        return "redirect:/login";
+        authenticate(user);
+        return "redirect:/profile";
+    }
+
+    private void authenticate(User user) {
+        UserDetails userDetails = new UserWithRoles(user, Collections.emptyList());
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                userDetails.getPassword(),
+                userDetails.getAuthorities()
+        );
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(auth);
     }
 
     @GetMapping("/profile")
     public String profile(){
         return "users/profile";
     }
-
 }
