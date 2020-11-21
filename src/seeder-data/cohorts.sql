60386c8155f770d37899a97a970da810ed1f801b
Dimitri <dma2012@gmail.com>
2020-11-20 18:46:29 -0600

    seeded cohorts with more data

--- src/seeder-data/cohorts.sql
+++ src/seeder-data/cohorts.sql
@@ -1,4 +1,8 @@
 use cohort_calendar_db;
 
 INSERT INTO cohorts(name, start_date, program_type, campus, contact_hours)
-VALUES ('Fortuna','2020-01-13','Web Dev','SAT',670),('Ganymede','2020-03-23','Web Dev','SAT',670);
+VALUES
+       ('Fortuna','2020-01-13','Web Dev','SAT',670),('Ganymede','2020-03-23','Web Dev','SAT',670),
+       ('Hyperion','2020-05-26','Web Dev','SAT',670),('Io','2020-07-20','Web Dev','SAT',670),
+       ('Bash','2020-07-20','Web Dev','DAL',670),('Darden','2020-07-13','Data Science','SAT',670)
+       ;
