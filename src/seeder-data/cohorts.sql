8f7192dc03ce8bbcc0978f7535c09d1f4051e2c5
Dimitri <dma2012@gmail.com>
2020-11-25 14:45:39 -0600

    updated cohorts seeder data

--- src/seeder-data/cohorts.sql
+++ src/seeder-data/cohorts.sql
@@ -2,7 +2,25 @@ use cohort_calendar_db;
 
 INSERT INTO cohorts(name, start_date, program_type, campus, contact_hours)
 VALUES
-       ('Fortuna','2020-01-13','Web Dev','SAT',670),('Ganymede','2020-03-23','Web Dev','SAT',670),
-       ('Hyperion','2020-05-26','Web Dev','SAT',670),('Io','2020-07-20','Web Dev','SAT',670),
-       ('Bash','2020-07-20','Web Dev','DAL',670),('Darden','2020-07-13','Data Science','SAT',670)
-       ;
+       ('Fortuna','2020-01-13','Web Dev','SAT',670),
+       ('Ganymede','2020-03-23','Web Dev','SAT',670),
+       ('Hyperion','2020-05-26','Web Dev','SAT',670),
+       ('Darden','2020-07-13','Data Science','SAT',670),
+       ('Bash','2020-07-20','Web Dev','DAL',670),
+       ('Io','2020-07-20','Web Dev','SAT',670),
+       ('Jupiter','2020-09-21','Web Dev','SAT',670),
+       ('Kalypso','2020-11-16','Web Dev','SAT',670),
+       ('Luna','2020-12-07','Web Dev','SAT',670),
+       ('Easley','2020-12-07','Data Science','SAT',670),
+       ('Cobol','2021-01-19','Web Dev','DAL',670),
+       ('Draco','2021-02-08','Web Dev','DAL',670),
+       ('Mars','2021-03-15','Web Dev','SAT',670),
+       ('Florence','2021-03-29','Data Science','SAT',670),
+       ('Neptune','2021-05-17','Web Dev','SAT',670),
+       ('Elixir','2021-06-14','Web Dev','DAL',670),
+       ('Oberon','2021-07-12','Web Dev','SAT',670),
+       ('Gauss','2021-07-19','Data Science','SAT',670),
+       ('Polaris','2021-09-13','Web Dev','SAT',670),
+       ('Quasar','2021-11-15','Web Dev','SAT',670),
+       ('Hopper','2021-11-15','Data Science','SAT',670)
+;
