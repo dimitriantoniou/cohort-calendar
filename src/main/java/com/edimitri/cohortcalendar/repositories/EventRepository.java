3bbefc8ffccc4ed4539cd02b8bac37cd3a1316fa
Dimitri <dma2012@gmail.com>
2020-11-27 09:44:00 -0600

    deleted calendar event features

--- src/main/java/com/edimitri/cohortcalendar/repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edimitri.cohortcalendar.repositories;
-
-import com.edimitri.cohortcalendar.models.Cohort;
-import com.edimitri.cohortcalendar.models.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event,Long> {
-    Cohort findBy(String name);
-}
